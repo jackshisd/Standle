b43972f069d822de5102c364ab7fb04f77d8158e
Jack Shi <jackshi.sd@gmail.com>
2024-02-09 19:17:14 -0800

    2/9/24

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: "./app.js",
-  output: {
-    path: path.resolve(__dirname, "dist"), // Output directory
-    filename: "bundle.js", // Output file name
-  },
-};
